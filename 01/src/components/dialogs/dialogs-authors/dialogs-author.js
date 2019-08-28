4e86187980695ff53d3a75d6fe61ee9c88e4f389
ASTOUND\o.taranov <o.taranov@astoundcommerce.com>
2019-08-28 18:58:01 +0300

    FIX: Name variable authorID

--- 01/src/components/dialogs/dialogs-authors/dialogs-author.js
+++ 01/src/components/dialogs/dialogs-authors/dialogs-author.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const DialogsAuthor = (props) => {
-    let path = "/dialogs/" + props.authorid;
+    let path = "/dialogs/" + props.idAuthor;
 
     return (
         <li className="b_dialogs__list_item">

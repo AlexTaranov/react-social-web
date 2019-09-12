44b268f86f88e14b2bdaf505d4c6cec4f429794a
ASTOUND\o.taranov <o.taranov@astoundcommerce.com>
2019-09-12 20:40:57 +0300

    ADD: Store - OOP

--- 01/src/index.js
+++ 01/src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
-import appState from './redux/state'
-import {addPost, updateNewPostText, addMsg, updateNewMessageText, subscribe} from "./redux/state";
+import store from "./redux/state";
 
 let rerenderPage = (appState) => {
     ReactDOM.render(
-        <App appState={appState} addPost={addPost} updateNewPostText={updateNewPostText} addMsg={addMsg} updateNewMessageText={updateNewMessageText} />,
+        <App
+            appState={appState}
+            addPost={store.addPost.bind(store)}
+            updateNewPostText={store.updateNewPostText.bind(store)}
+            addMsg={store.addMsg.bind(store)}
+            updateNewMessageText={store.updateNewMessageText.bind(store)} />,
         document.getElementById('root')
     );
 }
 
 
-rerenderPage(appState);
+rerenderPage(store.getState());
 
-subscribe(rerenderPage);
+store.subscribe(rerenderPage);
--- 01/src/redux/state.js
+++ 01/src/redux/state.js
@@ -1,83 +1,126 @@
-let rerenderPage = () => {
-    console.log('rerender Page');
-}
-
-let appState = {
-    profilePage: {
-        postData: [
-            {idPost: 1, message: 'Hello, its my first Post?', likesCount: "32", avatar: 'http://gameszon.net/content/cats-img/avatar3.png'},
-            {idPost: 2, message: 'Very good', likesCount: "25", avatar: 'https://writercenter.ru/uploads/images/01/80/70/2017/07/01/avatar_ratsh_194510_64x64.jpg'},
-            {idPost: 3, message: 'Very good 2222', likesCount: "31", avatar: 'http://gameszon.net/content/cats-img/avatar3.png'}
-        ],
-        profileData: [
-            {profileName: 'Alexander Taranov', profileInterest: 'Cars?', profilePosition: "FE DEV", profileAvatar: 'http://www.spletnik.ru/img/__post/68/68cd2b706c1fe59dc8df7e58a1655546_300.jpg'}
-        ],
-        newPostText: 'it-kamasutra.com'
+let store = {
+    _appState: {
+        profilePage: {
+            postData: [
+                {
+                    idPost: 1,
+                    message: 'Hello, its my first Post?',
+                    likesCount: "32",
+                    avatar: 'http://gameszon.net/content/cats-img/avatar3.png'
+                },
+                {
+                    idPost: 2,
+                    message: 'Very good',
+                    likesCount: "25",
+                    avatar: 'https://writercenter.ru/uploads/images/01/80/70/2017/07/01/avatar_ratsh_194510_64x64.jpg'
+                },
+                {
+                    idPost: 3,
+                    message: 'Very good 2222',
+                    likesCount: "31",
+                    avatar: 'http://gameszon.net/content/cats-img/avatar3.png'
+                }
+            ],
+            profileData: [
+                {
+                    profileName: 'Alexander Taranov',
+                    profileInterest: 'Cars?',
+                    profilePosition: "FE DEV",
+                    profileAvatar: 'http://www.spletnik.ru/img/__post/68/68cd2b706c1fe59dc8df7e58a1655546_300.jpg'
+                }
+            ],
+            newPostText: 'it-kamasutra.com'
+        },
+        messegesPage: {
+            dialogsData: [
+                {idAuthor: 1, nameauthor: 'Valera'},
+                {idAuthor: 2, nameauthor: 'Petro'},
+                {idAuthor: 3, nameauthor: 'Alexander'},
+                {idAuthor: 4, nameauthor: 'Vasiliy'},
+            ],
+            messageData: [
+                {
+                    authorava: 'https://wpjournalist.nl/wp-content/uploads/2019/03/avatar-jongen-voorbeeld-1.jpg',
+                    authorname: 'Valera',
+                    authormsg: 'Hello its my message for you!'
+                },
+                {
+                    authorava: 'https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg',
+                    authorname: 'Me',
+                    authormsg: 'Hey, men! Nice to meet you.'
+                },
+            ],
+            newMsgText: 'it-kamasutra.com-MSG'
+        },
+        sidebarData: {
+            sidebarFriends: [
+                {
+                    id: 1,
+                    link: '#',
+                    avatar: 'https://materiell.com/wp-content/uploads/2015/04/deven-small1.png',
+                    name: 'Alex1'
+                },
+                {
+                    id: 2,
+                    link: '#',
+                    avatar: 'https://materiell.com/wp-content/uploads/2015/04/deven-small1.png',
+                    name: 'Alex2'
+                },
+                {
+                    id: 3,
+                    link: '#',
+                    avatar: 'https://materiell.com/wp-content/uploads/2015/04/deven-small1.png',
+                    name: 'Alex3'
+                },
+                {
+                    id: 4,
+                    link: '#',
+                    avatar: 'https://materiell.com/wp-content/uploads/2015/04/deven-small1.png',
+                    name: 'Alex4'
+                }
+            ]
+        }
     },
-    messegesPage: {
-        dialogsData: [
-            {idAuthor: 1, nameauthor: 'Valera'},
-            {idAuthor: 2, nameauthor: 'Petro'},
-            {idAuthor: 3, nameauthor: 'Alexander'},
-            {idAuthor: 4, nameauthor: 'Vasiliy'},
-        ],
-        messageData: [
-            {authorava: 'https://wpjournalist.nl/wp-content/uploads/2019/03/avatar-jongen-voorbeeld-1.jpg', authorname: 'Valera', authormsg: 'Hello its my message for you!'},
-            {authorava: 'https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg', authorname: 'Me', authormsg: 'Hey, men! Nice to meet you.'},
-        ],
-        newMsgText: 'it-kamasutra.com-MSG'
+    getState() {
+       return  this._appState;
     },
-    sidebarData: {
-        sidebarFriends: [
-            {id: 1, link: '#', avatar: 'https://materiell.com/wp-content/uploads/2015/04/deven-small1.png', name: 'Alex1'},
-            {id: 2, link: '#', avatar: 'https://materiell.com/wp-content/uploads/2015/04/deven-small1.png', name: 'Alex2'},
-            {id: 3, link: '#', avatar: 'https://materiell.com/wp-content/uploads/2015/04/deven-small1.png', name: 'Alex3'},
-            {id: 4, link: '#', avatar: 'https://materiell.com/wp-content/uploads/2015/04/deven-small1.png', name: 'Alex4'}
-        ]
+    _rerenderPage() {
+        console.log('rerender Page');
+    },
+    addPost() {
+        let newPost = {
+            idPost: 5,
+            message: this._appState.profilePage.newPostText,
+            likesCount: '24',
+            avatar: 'https://writercenter.ru/uploads/images/01/80/70/2017/07/01/avatar_ratsh_194510_64x64.jpg'
+        }
+        this._appState.profilePage.postData.push(newPost);
+        this._appState.profilePage.newPostText = '';
+        this._rerenderPage(this._appState);
+    },
+    updateNewPostText(newText) {
+        this._appState.profilePage.newPostText = newText;
+        this._rerenderPage(this._appState);
+    },
+    addMsg() {
+        let newMsg = {
+            authorava: 'https://wpjournalist.nl/wp-content/uploads/2019/03/avatar-jongen-voorbeeld-1.jpg',
+            authorname: 'Me',
+            authormsg: this._appState.messegesPage.newMsgText
+        }
+
+        this._appState.messegesPage.messageData.push(newMsg);
+        this._appState.messegesPage.newMsgText = '';
+        this._rerenderPage(this._appState);
+    },
+    updateNewMessageText(newText) {
+        this._appState.messegesPage.newMsgText = newText;
+        this._rerenderPage(this._appState);
+    },
+    subscribe(observer) {
+        this._rerenderPage = observer;
     }
 }
 
-window.state = appState;
-
-export const addPost = () => {
-
-    let newPost = {
-        idPost: 5,
-        message: appState.profilePage.newPostText,
-        likesCount: '24',
-        avatar: 'https://writercenter.ru/uploads/images/01/80/70/2017/07/01/avatar_ratsh_194510_64x64.jpg'
-    }
-    appState.profilePage.postData.push(newPost);
-    appState.profilePage.newPostText = '';
-    rerenderPage(appState);
-};
-
-export const updateNewPostText = (newText) => {
-    appState.profilePage.newPostText = newText;
-    rerenderPage(appState);
-}
-
-
-export const addMsg = () => {
-    let newMsg = {
-        authorava: 'https://wpjournalist.nl/wp-content/uploads/2019/03/avatar-jongen-voorbeeld-1.jpg',
-        authorname: 'Valera',
-        authormsg: appState.messegesPage.newMsgText
-    }
-
-    appState.messegesPage.messageData.push(newMsg);
-    appState.messegesPage.newMsgText = '';
-    rerenderPage(appState);
-};
-
-export const updateNewMessageText = (newText) => {
-    appState.messegesPage.newMsgText = newText;
-    rerenderPage(appState);
-}
-
-export const subscribe = (observer) => {
-    rerenderPage = observer;
-}
-
-
-export default appState;
+window.store = store;
+export default store;

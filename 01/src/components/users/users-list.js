da20cdab86c62663a2262548ffc091bf69d25a5c
ASTOUND\o.taranov <o.taranov@astoundcommerce.com>
2019-11-02 10:59:05 +0200

    ADD: Users page - Finish without API

--- 01/src/components/users/users-list.js
+++ 01/src/components/users/users-list.js
@@ -34,30 +34,32 @@ let UsersList = (props) => {
         )
     }
 
+    let userList = props.users.map( u =>
+        <div className="b-user" key={u.userId}>
+            <div className="b-user_thumnail">
+                <figure className="b-user_thumnail-img">
+                    <img src={u.avatar} alt=""/>
+                </figure>
+                { u.userFollow
+                    ? <button className="b-user_thumnail-follow m-follow" onClick={() => { props.unfollow(u.userId) } } >Follow</button>
+                    : <button className="b-user_thumnail-follow m-unfollow" onClick={() => { props.follow(u.userId) } } >Unfollow</button>
+                }
+
+            </div>
+            <div className="b-user_info">
+                <div className="b-user_info-name">{u.userName}</div>
+                <div className="b-user_info-status">{u.userStatus}</div>
+                <div className="b-user_info-location">
+                    <div className="b-user_info-location-city">{u.userLocation.city}</div>
+                    <div className="b-user_info-location-country">{u.userLocation.country}</div>
+                </div>
+            </div>
+        </div>
+    )
+
     return (
         <div className='b-users-list'>
-            {props.users.map( u =>
-                <div className="b-user" key={u.userId}>
-                    <div className="b-user_thumnail">
-                        <figure className="b-user_thumnail-img">
-                            <img src={u.avatar} alt=""/>
-                        </figure>
-                        { u.userFollow
-                            ? <button className="b-user_thumnail-follow m-follow" onClick={() => { props.unfollow(u.userId) } } >Follow</button>
-                            : <button className="b-user_thumnail-follow m-unfollow" onClick={() => { props.follow(u.userId) } } >Unfollow</button>
-                        }
-
-                    </div>
-                    <div className="b-user_info">
-                        <div className="b-user_info-name">{u.userName}</div>
-                        <div className="b-user_info-status">{u.userStatus}</div>
-                        <div className="b-user_info-location">
-                            <div className="b-user_info-location-city">{u.userLocation.city}</div>
-                            <div className="b-user_info-location-country">{u.userLocation.country}</div>
-                        </div>
-                    </div>
-                </div>
-            )}
+            {userList}
         </div>
     );
 }

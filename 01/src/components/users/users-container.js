926b1b0a8d5177601315b9249cfcc4b99ce77e44
ASTOUND\o.taranov <o.taranov@astoundcommerce.com>
2019-11-20 18:54:25 +0200

    ADD: Refactering usersDispatchToProps

--- 01/src/components/users/users-container.js
+++ 01/src/components/users/users-container.js
@@ -15,17 +15,17 @@ class UsersListAPI extends  React.Component {
         this.props.toggleIsFetchingAC(true);
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(responce => {
             this.props.toggleIsFetchingAC(false);
-            this.props.setUsers(responce.data.items);
-            this.props.setTotalUsersCount(responce.data.totalCount);
+            this.props.setUsersAC(responce.data.items);
+            this.props.setUsersTotalCountAC(responce.data.totalCount);
         });
     };
 
     onChangePage = (page_number) => {
-        this.props.setCurrentPage(page_number);
+        this.props.setPageAC(page_number);
         this.props.toggleIsFetchingAC(true);
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page_number}&count=${this.props.pageSize}`).then(responce => {
             this.props.toggleIsFetchingAC(false);
-            this.props.setUsers(responce.data.items);
+            this.props.setUsersAC(responce.data.items);
         });
     };
 
@@ -39,8 +39,8 @@ class UsersListAPI extends  React.Component {
                     currentPage={this.props.currentPage}
                     onChangePage={this.onChangePage}
                     users={this.props.users}
-                    follow={this.props.follow}
-                    unfollow={this.props.unfollow}
+                    follow={this.props.userFollowAC}
+                    unfollow={this.props.userUnfollowAC}
                 />
             </div>
         );
@@ -58,28 +58,36 @@ let usersStateToProps = (state) => {
     }
 };
 
-let usersDispatchToProps = (dispatch) => {
-    return {
-        follow: (id) => {
-            dispatch(userFollowAC(id));
-        },
-        unfollow: (id) => {
-            dispatch(userUnfollowAC(id));
-        },
-        setUsers: (users) => {
-            dispatch(setUsersAC(users));
-        },
-        setCurrentPage: (page_number) => {
-            dispatch(setPageAC(page_number));
-        },
-        setTotalUsersCount: (totalCount) => {
-            dispatch(setUsersTotalCountAC(totalCount));
-        },
-        toggleIsFetchingAC: (isFetching) => {
-            dispatch(toggleIsFetchingAC(isFetching))
-        }
-    }
-}
+// let usersDispatchToProps = (dispatch) => {
+//     return {
+//         follow: (id) => {
+//             dispatch(userFollowAC(id));
+//         },
+//         unfollow: (id) => {
+//             dispatch(userUnfollowAC(id));
+//         },
+//         setUsers: (users) => {
+//             dispatch(setUsersAC(users));
+//         },
+//         setCurrentPage: (page_number) => {
+//             dispatch(setPageAC(page_number));
+//         },
+//         setTotalUsersCount: (totalCount) => {
+//             dispatch(setUsersTotalCountAC(totalCount));
+//         },
+//         toggleIsFetchingAC: (isFetching) => {
+//             dispatch(toggleIsFetchingAC(isFetching))
+//         }
+//     }
+// };
 
 
-export default connect(usersStateToProps, usersDispatchToProps)(UsersListAPI)
+
+export default connect(usersStateToProps, {
+    userFollowAC,
+    userUnfollowAC,
+    setUsersAC,
+    setPageAC,
+    setUsersTotalCountAC,
+    toggleIsFetchingAC
+})(UsersListAPI)
